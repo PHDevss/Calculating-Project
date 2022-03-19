b0f1988ad87666f7259a668a0a242acdeaed9f6a
PHDevss <97169087+PHDevss@users.noreply.github.com>
2022-03-19 15:17:41 -0300

    Finalizando Projeto

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 let num = document.querySelector('input#fnum')
-let flista = document.querySelector('select#flista')
+let lista = document.querySelector('select#flista')
 let res = document.querySelector('div#res')
+let valores = []
 
 function adicionar() {
-    if (isNumero(num.value) && !inLista(num.value)) {
-
+    if (isNumero(num.value) && !inLista(num.value, valores)) {
+        lista.style.display = "block"
+        valores.push(Number(num.value))
+        let item = document.createElement('option')
+        item.text =`Valor ${num.value} adicionado`
+        lista.appendChild(item)
+        res.innerHTML = ''
     } else {
-        window.alert('Valor inválido ou já encontrado na lista.')
+        window.alert('[ERRO] Valor inválido ou já encontrado na lista.')
     }
+    num.value = ''
+    num.focus()
 }
 
 function isNumero(n) {
@@ -23,5 +31,34 @@ function inLista(n, l) {
         return true
     } else {
         return false
+    }
+}
+
+function finalizar() {
+    if (valores.length==0){
+        window.alert('[ERRO] Adicione valores antes de finalizar.')
+    } else {
+        let tot = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma = 0
+        let media = 0
+        for(let pos in valores){
+            soma += valores[pos]
+            if(valores[pos]>maior){
+                maior = valores[pos]
+            }
+            if(valores[pos]<menor){
+                menor = valores[pos]
+            }
+        }
+        media = soma/tot
+        res.innerHTML = ''
+        res.innerHTML += `<p>Ao todo, temos ${tot} números cadastrados.</p>`
+        res.innerHTML += `<p>O maior valor informado foi ${maior}.</p>`
+        res.innerHTML += `<p>O menor valor informado foi ${menor}.</p>`
+        res.innerHTML += `<p>A soma de todos valores é ${soma}.</p>`
+        res.innerHTML += `<p>A média de todos valores é ${media}.</p>`
+
     }
 }
